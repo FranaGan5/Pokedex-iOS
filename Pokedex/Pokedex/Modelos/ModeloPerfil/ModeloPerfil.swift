d372b1e8bb59980d8ed9db5e6e4db63c5bfb4d6d
FranaGan <franag.dev@gmail.com>
2024-12-03 20:05:13 +0100

    Lista-modelo-perfil

---
+++ Pokedex/Pokedex/Modelos/ModeloPerfil/ModeloPerfil.swift
@@ -0,0 +1,38 @@
+//
+//  PerfilModel.swift
+//  Pokedex
+//
+//  Created by InnoTest-iOS on 3/12/24.
+//
+
+import Foundation
+
+struct ModeloPerfil: Identifiable{
+    let id = UUID()
+    let emoji: String
+    let nombre: String
+    let descripcion: String
+}
+let lista: [ModeloPerfil] = [
+    ModeloPerfil(
+           emoji: "📱",
+           nombre: "Teléfono",
+           descripcion: "Persona que levanta la mano para preguntar."),
+    ModeloPerfil(
+           emoji: "✉️",
+           nombre: "Email",
+           descripcion: "El aguacate es un fruto exótico carnoso que se obtiene del árbol tropical del mismo nombre. En algunas partes de América del Sur se conoce como Palta.."),
+    ModeloPerfil(
+           emoji: "🏠",
+           nombre: "Dirección",
+           descripcion: "Comida chatarra creada en Italia."),
+    ModeloPerfil(
+           emoji: "🪪",
+           nombre: "DNI",
+           descripcion: "Los rompecabezas o puzzles son piezas comúnmente planas que combinadas correctamente forman una figura, un objeto o una escena. ... Por diversión, entretenimiento o como una forma de relajarse"),
+    ModeloPerfil(
+           emoji: "👨🏽‍🌾",
+           nombre: "Alias",
+           descripcion: "Estatua ubicada en Nuva York."),
+    
+]
--- Pokedex/Pokedex/Preview Content/Perfil/PerfilView.swift
+++ Pokedex/Pokedex/Preview Content/Perfil/PerfilView.swift
@@ -9,10 +9,40 @@ import SwiftUI
 
 struct PerfilView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            ZStack{
+                Color.red.opacity(0.4)
+                    .edgesIgnoringSafeArea(.all)
+                List(lista){ item in
+                    HStack{
+                        emoji(emoji: item)
+                        Text(item.nombre)
+                            .bold()
+                    }
+                }
+                .scrollContentBackground(.hidden)
+                
+            }
+            
+        }
+    }
+   
+}
+struct emoji: View{
+    let emoji: ModeloPerfil
+    var body: some View{
+        ZStack{
+            Text(emoji.emoji)
+                .shadow(radius: 3)
+                .font(.largeTitle)
+                .frame(width: 65, height: 65)
+                .overlay(
+                    Circle()
+                        .stroke(Color.red, lineWidth: 3)
+                )
+        }
     }
 }
-
 #Preview {
     PerfilView()
 }
