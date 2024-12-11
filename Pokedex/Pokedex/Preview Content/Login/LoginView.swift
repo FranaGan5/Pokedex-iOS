6f71fb54d3c4e96ef780a9446d4b79d7af6f8c23
FranaGan <franag.dev@gmail.com>
2024-12-11 20:20:56 +0100

    Proceso-Login

--- Pokedex/Pokedex/Preview Content/Login/LoginView.swift
+++ Pokedex/Pokedex/Preview Content/Login/LoginView.swift
@@ -7,17 +7,50 @@
 
 import SwiftUI
 
+
+
+
 struct LoginView: View {
     var body: some View {
         ZStack{
             Color.yellow
+                .ignoresSafeArea(.all)
             VStack{
-               
+                Spacer()
+                Image("pokebola")
+                VStack{
+                    Button{
+                        print("login")
+                    }label: {
+                        Label("Accede por Email", systemImage: "envelope.fill")
+                        
+                    }
+                    .tint(.black)
+                }
+                .controlSize(.large)
+                .buttonStyle(.bordered)
+                .padding(.top, 40)
+                Spacer()
+                HStack{
+                    Button{
+                        print("registro")
+                    }label: {
+                        Text("¿No tienes cuenta?")
+                        Text("Registrate")
+                            .underline()
+                    }
+                    .tint(.black)
+                }
             }
+            
+            
+          
+            
+          
         }
-        
     }
 }
+
 
 #Preview {
     LoginView()
