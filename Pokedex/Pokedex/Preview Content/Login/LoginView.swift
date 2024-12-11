b4dd17a0a104b1df20d23e62b523e918232c9b68
FranaGan <franag.dev@gmail.com>
2024-12-11 19:36:27 +0100

    Fix LaunchScreen

---
+++ Pokedex/Pokedex/Preview Content/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  Pokedex
+//
+//  Created by InnoTest-iOS on 11/12/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack{
+            Color.yellow
+            VStack{
+               
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    LoginView()
+}
