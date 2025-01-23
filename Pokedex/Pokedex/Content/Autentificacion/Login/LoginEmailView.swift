3f88e8acac10b0055ddc17c95a53690d8b6b5224
FranaGan <franag.dev@gmail.com>
2025-01-23 21:19:39 +0100

    refact login

--- Pokedex/Pokedex/Content/Autentificacion/Login/LoginEmailView.swift
+++ Pokedex/Pokedex/Content/Autentificacion/Login/LoginEmailView.swift
@@ -17,76 +17,52 @@ struct LoginEmailView: View {
         ZStack {
             Color(Colors.amarillo.rawValue)
                 .ignoresSafeArea()
-            VStack {
-                Spacer()
-                ZStack {
-                    VStack {
-                        Rectangle()
-                            .frame(height: 40)
-                            .foregroundStyle(Color(Colors.marron.rawValue))
-                        Rectangle()
-                            .frame(height: 40)
-                            .foregroundStyle(Color(Colors.marron.rawValue))
-                            .padding(.top, 30)
-                    }
-                    Spacer()
-                    Image("cola-pikachu")
-                        .resizable()
-                        .frame(width: 350, height: 390)
-                        .padding(.top, 50)
-                }
-            }
 
             VStack(alignment: .center) {
                 CerrarView()
                     .padding(.top, 20)
                 
                 VStack(spacing: 40) {
-                    HStack {
-                        Text("¡Bienvenido!")
+                        
+                    Text("¡Bienvenido!")
                             .font(.system(size: 28, weight: .bold, design: .default))
                             .bold()
                             .foregroundColor(.black)
-                        Spacer()
-                    }
-                    .frame(maxWidth: 300)
+                            .frame(maxWidth: 180 , alignment: .center)
+                            .padding(.horizontal, 30)
                     
-                    HStack {
+                    HStack{
                         TextField("Email", text: $textFieldEmail)
-                            .frame(height: 40)
                             .padding(.leading, 10)
+                            .frame(width: 300, height: 40)
+                            .background(.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 6)
+                                    .stroke(Color(.black), lineWidth: 2))
                     }
-                    .background(.white)
-                    .frame(maxWidth: 300)
-                    .cornerRadius(6)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 6)
-                            .stroke(Color(.black), lineWidth: 2)
-                    )
-                    
-                    
+           
                     HStack {
-                        if mostrarContrasena{
-                            TextField("Contraseña", text: $textFieldContrasena)
-                                .frame(height: 40)
-                                .padding(.leading, 10)
-                        }else{
-                            SecureField("Contraseña", text: $textFieldContrasena)
-                                .frame(height: 40)
-                                .padding(.leading, 10)
+                        Group {
+                            if mostrarContrasena {
+                                TextField("Contraseña", text: $textFieldContrasena)
+                                    .padding(.leading, 10)
+                            } else {
+                                SecureField("Contraseña", text: $textFieldContrasena)
+                                    .padding(.leading, 10)
+                            }
                         }
+                        .frame(height: 40)
+                        .cornerRadius(6)
                         Button(action:{
                             mostrarContrasena.toggle()
                         }){
                             Image(systemName: mostrarContrasena ? "eye" : "eye.slash")
                                 .foregroundStyle(.gray)
                                 .padding(.trailing, 10)
+                                
                         }
-                        
-                    }
+                    }.frame(width: 300)
                     .background(.white)
-                    .frame(maxWidth: 300)
-                    .cornerRadius(6)
                     .overlay(
                         RoundedRectangle(cornerRadius: 6)
                             .stroke(Color(.black), lineWidth: 2)
